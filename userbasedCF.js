d8a87629bd09462e51a7c3d5417187d5be8fc759
piekey1994 <xiaoshendidi@163.com>
2017-03-23 23:34:42 +0800

    新增皮尔逊距离计算

--- userbasedCF.js
+++ userbasedCF.js
@@ -4,6 +4,7 @@ var db = new sqlite3.Database('test.db');
 var Promise = require('bluebird');
 var async=require('async');
 
+
 //查找和当前用户有交集的所有用户
 function findOther(uid)
 {
@@ -62,9 +63,63 @@ function createDict(uids)
         });
 }
 
+//求和
+Array.prototype.sum = function (){
+ return this.reduce(function (partial, value){
+  return partial + value;
+ })
+};
+Array.prototype.pow = function(){
+    newArray=[];
+    for(var item in this)
+    {
+        newArray.push(Math.pow(item,2));
+    }
+    return newArray;
+};
+
 //皮尔逊距离
 function sim_pearson(dict, p1, p2)
 {
+    si=[];
+    for(var item in dict[p1]) 
+    {
+        if(item in dict[p2])
+        {
+            si.push(item);
+        }
+    }
+    n=si.length;
+    if(n==0) return 1;
+
+    list1=[];
+    list2=[];
+    for(var item in si)
+    {
+        list1.push(dict[p1][item]);
+        list2.push(dict[p2][item]);
+    }
+
+    sum1=list1.sum();
+    sum2=list2.sum();
+
+    sum1Sq=list1.pow().sum();
+    sum2Sq=list2.pow().sum();
+
+    plist=[];
+    for(var item in si)
+    {
+        plist.push(dict[p1][item]*dict[p2][item]);
+    }
+    pSum=plist.sum();
+
+    num=pSum - (sum1*sum2/n);
+    den = Math.sqrt((sum1Sq - Math.pow(sum1, 2)/n) * (sum2Sq - Math.pow(sum2, 2)/n));
+    if(den==0) return 0;
+
+    r=num/den;
+
+    return r;
 
 }
 
